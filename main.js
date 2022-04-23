855713379c54d1755652c95466c1237811ccaa86
TSC7 <michaelsantos1337@gmail.com>
2022-04-23 11:32:18 -0400

    Added zeros when appropriate

--- main.js
+++ main.js
@@ -29,6 +29,12 @@ let digit = document.querySelectorAll(".digit");
 let display = document.querySelector(".screen");
 let button = document.querySelector(".button");
 let point = document.querySelector(".point.digit");
+let operation = document.querySelectorAll(".operation");
+let divide = document.querySelector(".operation.divide");
+let multiply = document.querySelector(".operation.multiply");
+let add = document.querySelector(".operation.add");
+let subtract = document.querySelector(".operation.subtract");
+let evaluate = document.querySelector(".equals");
 
 window.addEventListener("DOMContentLoaded", () => {
   digit.forEach(button => {
@@ -37,12 +43,53 @@ window.addEventListener("DOMContentLoaded", () => {
        let integer = `<p>${parseInt(e.target.textContent)}</p>`
        display.innerHTML += integer;
      } else {
+      if (display.textContent == "") {
+        display.innerHTML = "<p>0</p>";
+      }
        let float = `<p>.</p>`
        display.innerHTML += float;
        if ((/[.]$/.test(display.textContent)) == true) {
          point.disabled = "disabled";
        }
+       if ((/(\+|x|-)[.]/.test(display.textContent)) == true) {
+        let position = (/(\+|x|-)[.]/.exec(display.textContent).index) + 1;
+        display.innerHTML = `<p>${[display.textContent.slice(0, position), "0",
+        display.textContent.slice(-1)].join("")}`;
+      }
      }     
     })
   })
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+  operation.forEach(button => {
+    button.addEventListener("click", function (e) {
+      if (display.textContent == "") {
+        display.innerHTML = "<p>0</p>";
+      }
+      let operation = `<p>${e.target.textContent}</p>`;
+      display.innerHTML += operation;
+      if (e.target.classList[1] == "add") {
+        add.disabled = "disabled";
+        subtract.disabled = "disabled";
+        multiply.disabled = "disabled";
+        divide.disabled = "disabled";
+      } else if (e.target.classList[1] == "subtract") {
+        add.disabled = "disabled";
+        subtract.disabled = "disabled";
+        multiply.disabled = "disabled";
+        divide.disabled = "disabled";
+      } else if (e.target.classList[1] == "multiply") {
+        add.disabled = "disabled";
+        subtract.disabled = "disabled";
+        multiply.disabled = "disabled";
+        divide.disabled = "disabled";
+      } else if (e.target.classList[1] == "divide") {
+        add.disabled = "disabled";
+        subtract.disabled = "disabled";
+        multiply.disabled = "disabled";
+        divide.disabled = "disabled";
+      }
+    })
+  })
+})
